3634f7b85562ae2326a86eca12fff008445a32ea
Harsh Raj <harshbasketball06@gmail.com>
2023-12-27 15:15:23 +0530

    initial commit

---
+++ QuizNest/src/main/java/com/quiznestserver/QuizNestApplication.java
@@ -0,0 +1,13 @@
+package com.quiznestserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizNestApplication.class, args);
+	}
+
+}
---
+++ QuizNest/src/test/java/com/quiznestserver/QuizNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiznestserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
