da180455de6581cc0e415aa088018a5d71a76d52
Harsh Raj <harshbasketball06@gmail.com>
2024-01-03 21:18:32 +0530

    initial commit

---
+++ quiznest-frontend/src/app/components/user-registeration/user-registeration.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-registeration',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-registeration.component.html',
+  styleUrl: './user-registeration.component.css'
+})
+export class UserRegisterationComponent {
+
+}
