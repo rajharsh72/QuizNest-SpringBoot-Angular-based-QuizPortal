c98aea8193a9f1d2c3cc55b725f2f7bcd6b1f142
Harsh Raj <harshbasketball06@gmail.com>
2024-01-04 20:17:51 +0530

    initial commit

--- quiznest-frontend/src/app/app.routes.ts
+++ quiznest-frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { UserRegisterationComponent } from './components/user-registeration/user-registeration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'register', component: UserRegisterationComponent, pathMatch:'full'},
+];
--- quiznest-frontend/src/app/components/user-registeration/user-registeration.component.ts
+++ quiznest-frontend/src/app/components/user-registeration/user-registeration.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 @Component({
   selector: 'app-user-registeration',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule
+            ,MatFormFieldModule,
+          MatInputModule],
   templateUrl: './user-registeration.component.html',
   styleUrl: './user-registeration.component.css'
 })
