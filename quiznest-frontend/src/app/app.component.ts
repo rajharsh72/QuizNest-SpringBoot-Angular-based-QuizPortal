389ef78b94c279cb138bf471d71441a6ba86ee93
Harsh Raj <harshbasketball06@gmail.com>
2024-01-17 21:09:12 +0530

    initial commit

--- quiznest-frontend/src/app/app.component.ts
+++ quiznest-frontend/src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './shared/navbar/navbar.component';
+
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'quiznest-frontend';
--- quiznest-frontend/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { UserRegisterationComponent } from './components/user-registeration/user-registeration.component';
-
-export const routes: Routes = [
-    {path:'register', component: UserRegisterationComponent, pathMatch:'full'},
-];
