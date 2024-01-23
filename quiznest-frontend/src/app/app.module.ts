3c1e1e04f04d8062cd7ce232ed2dffd82e00f10f
Harsh Raj <harshbasketball06@gmail.com>
2024-01-23 15:27:41 +0530

    initial commit

--- quiznest-frontend/src/app/app.module.ts
+++ quiznest-frontend/src/app/app.module.ts
@@ -17,6 +17,7 @@ import {MatIconModule} from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -39,7 +40,8 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     MatIconModule,
     FormsModule,
     HttpClientModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- quiznest-frontend/src/app/components/user-registeration/user-registeration.component.ts
+++ quiznest-frontend/src/app/components/user-registeration/user-registeration.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { UserRegisterationServiceService } from 'src/app/services/user-registeration-service.service';
 import Swal from 'sweetalert2';
@@ -33,13 +34,22 @@ export class UserRegisterationComponent {
     password:''
   };
 
+  registerationForm = new FormGroup({
+    'email': new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
+    'firstName': new FormControl('',[Validators.required, Validators.pattern('[a-zA-Z ]*')]),
+    'lastName':new FormControl('', Validators.pattern('[a-zA-Z ]*')),
+    'phoneNo': new FormControl('', [Validators.required, Validators.pattern('[0-9]{10}')]),
+    'gender': new FormControl('', Validators.required),
+    'password':new FormControl('', Validators.required)
+  })
+
 
   registerUser(){
-    console.log(this.user);
-      this._registerationService.registerUser(this.user).subscribe((res)=>{
+    if(this.registerationForm.valid){
+      this._registerationService.registerUser(this.registerationForm.value).subscribe((res)=>{
         console.log("user registered successfully")
         Swal.fire({
-          title: "Kudos" + ' '+ this.user.firstName,
+          title: "Kudos" + ' '+ this.registerationForm.value['firstName'],
           text: "Registeration Successfull",
           icon: "success"
         });
@@ -49,6 +59,13 @@ export class UserRegisterationComponent {
         this._snackBar.open("something went wrong... !!!", 'OK',{
           duration: 5000
         });
-      })
+      });
+    }
+    else{
+      this._snackBar.open("Invalid or missing details. Kindly check again",'OK',{
+        duration:5000
+      });
+    }
+      
   }
 }
